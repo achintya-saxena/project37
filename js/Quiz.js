80661f259ea85366287cd0862b95f4cca4349335
achintya-saxena <74810469+achintya-saxena@users.noreply.github.com>
2021-03-30 12:41:54 +0530

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,72 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref(contestantCount).once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+    question.hide();
+
+    //write code to change the background color here
+  background("yellow");
+    //write code to show a heading for showing the result of Quiz
+fill(0);
+textSize(30);
+text("Result of the Quiz",340,50);
+text("-------------------------",320,65);
+    //call getContestantInfo( ) here
+Contestant.getPlayerInfo();
+if(allContestants !==undefined) {
+debugger;
+var display_Answers=230;
+fill("blue");
+textSize(20);
+text("THOSE CONTESTANTS WHO ANSWERED CORRECT ARE HIGHLIGHTED IN GREEN COLOUR",50,230);
+
+for(var plr in allContestants) {
+  debugger;
+  var correctAnswer="2";
+  if(correctAnswer===allContestants[plr].answer) {
+    fill("green");
+  }
+    else{
+fill("red");
+    }
+display_Answers+=30
+textSize(20);
+text(allContestants[plr].name + ":" + allContestants[plr].answer,250,display_Answers);
+    
+  }
+
+}
+}
+    
+    
+  }
+
+
